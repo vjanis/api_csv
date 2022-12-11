7e3b5ddca95fe6621f6e18ff30c4ae4c8c0a6758
vjanis <33664266+vjanis@users.noreply.github.com>
2022-12-11 17:31:50 +0200

    papildināts ar datumu
    .

--- db_faili/models.py
+++ db_faili/models.py
@@ -14,6 +14,7 @@ class Book(Base):
     nosaukums = Column(String)
     gads = Column(String)
     periods = Column(String)
+    datums = Column(String)
     sadalits = Column(Float)
     pfif = Column(Float)
 
@@ -21,5 +22,5 @@ class Book(Base):
 #    published = Column(Date)
 
     def __repr__(self):
-        return "<Book(tips='{}', atvk='{}', nosaukums={}, gads={}, periods={}, sadalits={}, pfif={})>" \
-            .format(self.tips, self.atvk, self.nosaukums, self.gads, self.periods, self.sadalits, self.pfif)
+        return "<Book(tips='{}', atvk='{}', nosaukums={}, gads={}, periods={}, datums={}, sadalits={}, pfif={})>" \
+            .format(self.tips, self.atvk, self.nosaukums, self.gads, self.periods, self.datums, self.sadalits, self.pfif)
