50c5a46af13afb2fb774162a18150b321911cbda
vjanis <33664266+vjanis@users.noreply.github.com>
2022-12-11 02:43:47 +0200

    pareiza bāze

--- db_faili/config.py
+++ db_faili/config.py
@@ -2,4 +2,4 @@
 
 # Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
 
-DATABASE_URI = 'postgresql://postgres:adminadmin@api_db_image:5432/books'
+DATABASE_URI = 'postgresql://postgres:adminadmin@api_db_image:5432/postgres'
