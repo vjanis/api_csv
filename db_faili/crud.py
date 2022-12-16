ea76e044f44b06f13b01679cf18142146092286b
vjanis <jv19@inbox.lv>
2022-12-16 21:41:20 +0200

    no pycharm
    init fails

--- db_faili/crud.py
+++ db_faili/crud.py
@@ -2,8 +2,8 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from config import *
-from models import *
+from db_faili.config import *
+from db_faili.models import *
 
 engine = create_engine(DATABASE_URI)
 
